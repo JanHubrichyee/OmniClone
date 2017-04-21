308c5ed50f628dc23bef91999d2bb5704e8a18f1
JanHubrichyee <jan.hubrich@outlook.de>
2017-04-21 19:56:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,81 +1,58 @@
-import datetime, json, os
-
-actions = []
-inbox = []
-projects = []
-tags = []
-
-def saveToFile(list, PATH):
-	f = open(PATH, 'w')
-	if PATH in ['actions.txt', 'inbox.txt', 'projects.txt']:
-		for item in list:
-			json_str = json.dumps(item.__dict__)		# convert data to encoded string
-			f.write(json_str + '\n')			# write encoded string to .txt
-	elif PATH in ['tags.txt']:
-		for item in list:
-			f.write(item + '\n')
-	f.close()
-
-def loadFromFile(PATH):
-	list = []
-	f = open(PATH, 'r')
-	if PATH in ['actions.txt', 'inbox.txt', 'projects.txt']:
-		for line in f.readlines():			
-			list.append(json.loads(line[0:-1]))		# Load json from file into var
-	elif PATH in ['tags.txt']:
-		for line in f.readlines():
-			list.append(line[0:-1])
-	return list
-	f.close()
-
-class Project():
-	def __init__(self):
-		self.completed = False
-		self.title = ''
-		self.tags = []
-		self.deferUntil = ''
-		self.due = ''
-		self.estTime = ''
-		self.comment = ''
-		self.flagged = False
-		self.dateAdded = ''
-		self.dateChanged = ''
-		# Repeat settings
-		# Review settings
-		# Initialize displayNr (append as last item in displayed list)
-
-class Action(Project):
-	def __init__(self, project):
-		super(Action, self).__init__()
-		# What about actions without projects?
-		self.project = project
-
-def main():
-	# Load contents into variables
-	actions = loadFromFile('actions.txt')
-	inbox = loadFromFile('inbox.txt')
-	projects = loadFromFile('projects.txt')
-	tags = loadFromFile('tags.txt')
+import tkinter as tk
+from tkinter import ttk
 
 
-	
+#Fonts
+LARGE_FONT = ("Verdana", 12)
 
 
+class App(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.iconbitmap(self, default="Icon.ico")
+        tk.Tk.wm_title(self, "OmniClone")
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Omni Clone", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        #Startpage stuff
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page One", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        #nextPage stuff
 
 
-
-
-
-
-
-
-
-
-
-	# Write to files
-	saveToFile(actions, 'actions.txt')
-	saveToFile(inbox, 'inbox.txt')
-	saveToFile(projects, 'projects.txt')
-	saveToFile(tags, 'tags.txt')
-	# Create backups periodically 
-main()
+root = App()
+root.mainloop()
